517f613e7c6cddeeaeefec19bc2f8439eb717124
Lucas Lage <lucasflage@hotmail.com>
2018-12-05 11:31:38 -0200

    Asap_essay.py não é uma classe, se torna um módulo com funções

--- ASAP_essay.py
+++ ASAP_essay.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Sep 15 15:54:26 2018
+Created on Fri Nov 23 21:33:32 2018
 
 @author: lucas
 """
-from py4j.java_gateway import JavaGateway
+
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 import scipy
@@ -12,184 +12,112 @@ import re
 import nltk.data
 from textblob import Word
 import expandContractions as eC
-from textstat.textstat import textstat
 import discourseMarkers
 import Prompts
-# =============================================================================
-# import requests
-# =============================================================================
 
 
+def pre_process(essay):
+    
+    """Função que expande contrações, põe em caixa baixa e retira whitespace a mais"""
+        
+    essay_proc = eC.expandContractions(essay.lower())
+    essay_proc = re.sub(' +', ' ',essay_proc)
+    return essay_proc
 
-class ASAP_essay():
-    """classe para extração de features em uma redação do database ASAP"""
+def token_features(essay):
     
-    def __init__(self,essay,prompt_number):     # entra com o essay e o essay_set do DataFrame
+    """ Utiliza o objeto Vectorizer para extrair features relacionados a tokens"""
+    vector = CountVectorizer()
+    tokenized_essay = vector.fit_transform([essay])
+    
+    """Feature 1: Número de Tokens"""
+    nb_of_tokens = tokenized_essay.toarray().sum()
+    
+    """Feature 2: Media de caracteres por palavra"""
+    nb_of_char = 0
+    for position,item in enumerate(vector.get_feature_names()):
+        nb_of_char += len(item)* tokenized_essay.toarray()[0][position]			# multiplica o tamanho da string(token) pela sua frequência
+    mean_char_p_word = nb_of_char/nb_of_tokens
+    
+    """Feature 3: Número de palavras diferentes """
+    nb_dif_words = len(vector.get_feature_names())
+        
+    return (nb_of_tokens,mean_char_p_word,nb_dif_words)	
 
-        
-        self.essay = essay
-        self.prompt_number = prompt_number
-        self.nb_of_tokens = 0
-        self.cosine_distance = 0
-        self.nb_of_grammar_errors = 0
-        self.nb_of_spell_errors = 0
-        self.nb_of_optimal_sentences = 0
-        self.mean_char_p_word = 0
-        self.flesch_score = 0
-        self.nb_of_discourse_markers = 0
-        self.nb_of_diff_words = 0
-        self.grammar_errors_p_token = 0
-        self.spell_errors_p_token = 0
-        self.discourse_markers_p_sentence = 0
-        self.style_errors = 0
-        
-        to_tokenize = eC.expandContractions(self.essay.lower())
-        to_tokenize = re.sub(' +', ' ',to_tokenize)
-        self.vector = CountVectorizer()
-        self.tokenized_essay = self.vector.fit_transform([to_tokenize])
-        
-        """Inicializa o LanguageTool para recolher erros"""
-        self.gg = JavaGateway.launch_gateway(classpath="./Java/errorslanguagetool.jar;./Java/LanguageTool-4.3;./Java/py4j0.10.8.1.jar;./Java/LanguageTool-4.3/languagetool.jar")
-        self.extract = self.gg.jvm.languagetoolEnglish.ExtractErrors()
-        self.errors = self.extract.getErrors()
-        self.errors.process(self.essay)
-        
-        
-    def get_nb_of_tokens(self):
-        self.nb_of_tokens = self.tokenized_essay.toarray().sum()						# Soma todas as frequências de tokens dadas pelo CountVectorizer()
-        return (self.tokenized_essay.toarray().sum())	
-        
-    def get_mean_char_p_word(self):
-        if self.nb_of_tokens == 0:
-            print('Você deve ter o numero de tokens primeiro')
-        else:
-            nb_of_char = 0
-            for position,item in enumerate(self.vector.get_feature_names()):
-                nb_of_char += len(item)* self.tokenized_essay.toarray()[0][position]			# multiplica o tamanho da string(token) pela sua frequência
-            self.mean_char_p_word = nb_of_char/self.nb_of_tokens
-        return self.mean_char_p_word
-        
-        
-    def get_nb_of_diff_words(self):
-        self.nb_of_diff_words = len(self.vector.get_feature_names())							# lista de palavras dieferentes fornecidas pelo CountVectorizer
-        return (self.nb_of_diff_words)
-        
-    def get_nb_of_optimal_sentences(self):
-        counter = 0
-        sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')						# Sentence Tokenizer do NLKT
-        essay_sentence_list = sent_detector.tokenize(self.essay)			
-		  # essay_sentence_list é uma Lista de tokens(frases)
-        for i in essay_sentence_list:
-            n_words_sentence = len(i.split())
-            if n_words_sentence >= 15 and n_words_sentence <= 20:
-                counter +=1
-        self.nb_of_optimal_sentences = counter
-        return self.nb_of_optimal_sentences
-        
-    def get_nb_of_speliing_errors(self):
-        spell_errors = 0
-        new_vec = CountVectorizer()
-        to_tokenize = eC.expandContractions(self.essay.lower())
-        to_tokenize = re.sub(' +', ' ',to_tokenize)
-        to_tokenize = re.sub('@\S+','',to_tokenize)
-        
-        new_vec.fit_transform([to_tokenize])
-        
-        list_tokens = new_vec.get_feature_names()
-        
-        for i in list_tokens:
-            __result = None
-            __result = re.search('@\S+', i) 										      # retira nomes padrão do ASAP como @Location
-            if __result is None:
-                w = Word(i)														           	# da biblioteca textblob
-                result_tuple = w.spellcheck()									         	# returna uma tupla com a sugestão de correção e o nível de certeza em %
-                if i != result_tuple[0][0] and result_tuple[0][1] == 1.0:			# só faz correções que o corretor tem certeza, evita sugestão de singular em formas plurais
-                    spell_errors += 1
-        self.nb_of_spell_errors = spell_errors
-        return self.nb_of_spell_errors
+def nb_of_speliing_errors(essay):
     
-    def get_flesch_score(self):
-        self.flesch_score = textstat.flesch_reading_ease(self.essay)
-        return self.flesch_score
-        
-    def get_cos_dist(self):
-        # Retorna a distância cosseno de dois textos usando Scikit
-        __vectorizer = CountVectorizer()                                    		  
-        red_promp = [self.essay,Prompts.get_prompt(self.prompt_number)]      		      # O CountVectorizer() cria duas arrays de frequências de palavras do
-        y = __vectorizer.fit_transform(red_promp)                          		      # vocabulário do essay e do prompt
-        x = y.toarray()
-        transformer = TfidfTransformer(smooth_idf=False)
-        tfidf = transformer.fit_transform(x)										             # Faz as operações do TF.IDF
-        z = tfidf.toarray()
-        self.cosine_distance = scipy.spatial.distance.cosine(z[0], z[1])
-        return self.cosine_distance
+    """Feature 4: Utiliza a bilbioteca textblob para correção de erros de ortografia"""
+    essay = re.sub('@\S+','',essay)                                                 # Retira nomes padrão do ASAP iniciados em @
     
-# =============================================================================
-#     def get_nb_of_grammar_errors(self):
-#         url = 'https://languagetool.org/api/v2/check'
-#         r = requests.post(url, data = {'text':self.essay,
-#                                        'language': 'en',
-#                                        'disabledRules':'EN_QUOTES,WHITESPACE_RULE,COMMA_PARENTHESIS_WHITESPACE,SENTENCE_WHITESPACE' } )
-#         print('Status code:',r.status_code)
-#         response_dict = r.json()
-#         self.nb_of_grammar_errors = (len(response_dict['matches']))
-#         return self.nb_of_grammar_errors
-# =============================================================================
+    vector = CountVectorizer()
+    vector.fit_transform([essay])
     
-        
-    def get_nb_of_grammar_errors_LT(self):
-        self.nb_of_grammar_errors = self.errors.getOthersErrors()
-        return self.nb_of_grammar_errors
-        
-    def get_nb_of_discourse_markers(self):
-        counter=0
-        
-        for discourse_marker in discourseMarkers.multi_word_exp():
-            result = None
-            essay = self.essay
-            result = discourse_marker.findall(essay.lower())
-            re.sub(discourse_marker,'',essay)
-            counter += len(result)
-        
-        for discourse_marker in discourseMarkers.discourseMarkers():
-            matches = None
-            searchable_pattern = "\W" + discourse_marker + '\W'
-            matches = re.findall(searchable_pattern,essay.lower())
-            if matches != None:
-                counter += len(matches)
-        self.nb_of_discourse_markers = counter
-        return self.nb_of_discourse_markers
+    list_tokens = vector.get_feature_names()
     
-    
-    def get_grammar_errors_p_token_n(self):
-        self.grammar_errors_p_token = self.nb_of_grammar_errors/self.nb_of_tokens
-        return self.grammar_errors_p_token
-    
-    def get_n_spell_errors_p_token_n(self):
-        self.spell_errors_p_token = self.nb_of_spell_errors/self.nb_of_tokens
-        return self.spell_errors_p_token
-    
-    def get_n_discourse_markers_p_sentence(self):
-        sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')					
-        n_of_sentences = len(sent_detector.tokenize(self.essay))
-        self.discourse_markers_p_sentence = self.nb_of_discourse_markers/n_of_sentences
-        return self.discourse_markers_p_sentence
-    
-    def get_style_errors_LT(self):
-        return self.errors.getStyleErrors()
-    
-# =============================================================================
-#     def get_style_errors(self):
-#          url = 'https://languagetool.org/api/v2/check'
-#          r = requests.post(url, data = {'text':self.essay,
-#                                    'language': 'en',
-#                                    'enabledCategories':'STYLE',
-#                                    'disabledRules':'EN_QUOTES,WHITESPACE_RULE,COMMA_PARENTHESIS_WHITESPACE,SENTENCE_WHITESPACE',
-#                                    })
-#          print('Status code:',r.status_code)
-#          response_dict = r.json()
-#          self.style_errors = len(response_dict['matches'])
-#          return self.style_errors
-# =============================================================================
+    spell_errors = 0    
+    for i in list_tokens:
+        __result = None
+        __result = re.search('@\S+', i) 										# retira nomes padrão do ASAP como @Location
+        if __result is None:
+            w = Word(i)														    # da biblioteca textblob
+            result_tuple = w.spellcheck()									    # o método spellcheck() returna uma tupla com a sugestão de correção e o nível de certeza em %
+            if i != result_tuple[0][0] and result_tuple[0][1] == 1.0:			# só faz correções que o corretor tem certeza, 100%, evita sugestão de singular em formas plurais
+                spell_errors += 1
+    nb_of_spell_errors = spell_errors
+    return nb_of_spell_errors
 
+def get_cos_dist(essay,prompt_number):
+    
+    """Feature 5: Retorna a distância cosseno de dois textos usando Scikit"""
+    __vectorizer = CountVectorizer()                                    		  
+    red_promp = [essay,Prompts.get_prompt(prompt_number)]      		              # O CountVectorizer() cria duas arrays de frequências de palavras do
+    y = __vectorizer.fit_transform(red_promp)                          		      # vocabulário do essay e do prompt
+    x = y.toarray()
+    transformer = TfidfTransformer(smooth_idf=False)
+    tfidf = transformer.fit_transform(x)										  # Faz as operações do TF.IDF
+    z = tfidf.toarray()
+    cosine_distance = scipy.spatial.distance.cosine(z[0], z[1])
+    return cosine_distance
+
+def discourse_markers_features(essay):
+    
+    """Feature 6: Numero de marcadores de discurso na redação"""
+    
+    counter=0
+    for discourse_marker in discourseMarkers.multi_word_expression():
+        result = None
+        result = discourse_marker.findall(essay)
+        re.sub(discourse_marker,'',essay)
+        counter += len(result)
         
+    for discourse_marker in discourseMarkers.discourseMarkers():
+        matches = None
+        searchable_pattern = "\W" + discourse_marker + '\W'
+        matches = re.findall(searchable_pattern,essay.lower())
+        if matches != None:
+            counter += len(matches)
+    nb_of_discourse_markers = counter
+    
+    """Feature 7: Número de marcadores de discurso por frase"""
+    
+    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')					
+    n_of_sentences = len(sent_detector.tokenize(essay))
+    discourse_markers_p_sentence = nb_of_discourse_markers/n_of_sentences
+    
+    return (nb_of_discourse_markers,discourse_markers_p_sentence)
+
+def nb_optimal_sentence(essay):
+    
+    """Feature 8: Numero de frases com 15 a 20 palavras"""
+    counter = 0
+    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')						# Sentence Tokenizer do NLKT
+    essay_sentence_list = sent_detector.tokenize(essay)			
+	  # essay_sentence_list é uma Lista de tokens(frases)
+    for i in essay_sentence_list:
+        n_words_sentence = len(i.split())
+        if n_words_sentence >= 15 and n_words_sentence <= 20:
+            counter +=1
+        nb_of_optimal_sentences = counter
+    return nb_of_optimal_sentences
+
+
--- Get_features_to_CSV.py
+++ Get_features_to_CSV.py
@@ -4,36 +4,63 @@ Created on Tue Oct  9 10:07:34 2018
 
 @author: lucas
 """
-import pandas as pd
-from ASAP_essay import ASAP_essay
-# import time
 
+
+import pandas as pd
+#from ASAP_essay import ASAP_essay
+import ASAP_essay as asap
+from textstat.textstat import textstat
+from py4j.java_gateway import JavaGateway
+
+# Primeiramente iniciamos a conexão com java para extração de erros gramaticais com o Language Tool
+gg = JavaGateway.launch_gateway(classpath="./Java/errorslanguagetool.jar;./Java/LanguageTool-4.3;./Java/py4j0.10.8.1.jar;./Java/LanguageTool-4.3/languagetool.jar")
+
+
+
+# Leitura do dataset ASAP 
 valid_set = pd.read_csv("./Dados ASAP/training_set_rel3.tsv", sep = '\t+', engine = 'python')
 
 feature_df = pd.DataFrame()
 
-for index, row in valid_set[:100].iterrows():
-    X = ASAP_essay(valid_set['essay'][index],valid_set['essay_set'][index])
+for index, row in valid_set.iterrows():
+    """Colocando o id de cada essay no dataframe"""
     feature_df.loc[index,'essay_id'] = valid_set['essay_id'][index]
-    feature_df.loc[index,'nb_of_tokens'] = X.get_nb_of_tokens()
-    feature_df.loc[index,'mean_char_p_words'] = X.get_mean_char_p_word()
-    feature_df.loc[index,'diff_words'] = X.get_nb_of_diff_words()
-    feature_df.loc[index,'optimal_sentences'] = X.get_nb_of_optimal_sentences()
-    feature_df.loc[index,'nb_of_spell_errors'] = X.get_nb_of_speliing_errors()
-    feature_df.loc[index,'flesch_score'] = X.get_flesch_score()
-    feature_df.loc[index,'cos_dist'] = X.get_cos_dist()
     
-    feature_df.loc[index,'style_errors'] = X.get_style_errors_LT()    
-    feature_df.loc[index,'grammar_errors'] = X.get_nb_of_grammar_errors_LT()
+    """pre processando o essay"""
+    Y = asap.pre_process(valid_set['essay'][index])
     
-    feature_df.loc[index,'nb_of_spell_errors'] = X.get_nb_of_speliing_errors()
-    feature_df.loc[index,'discourse_markers'] = X.get_nb_of_discourse_markers()
-    feature_df.loc[index,'grammar_errors_p_token'] = X.get_grammar_errors_p_token_n()
-    feature_df.loc[index,'spell_errors_p_token'] = X.get_n_spell_errors_p_token_n()
-    feature_df.loc[index,'discourse_markers_p_sentence'] = X.get_n_discourse_markers_p_sentence()
+    """extração de features sobre tokens"""
+    token_feature_tp = asap.token_features(Y)
+    feature_df.loc[index,'nb_of_tokens'] = token_feature_tp[0]                  #F1
+    feature_df.loc[index,'mean_char_p_words'] = token_feature_tp[1]             #F2
+    feature_df.loc[index,'diff_words'] = token_feature_tp[2]                    #F3
     
+    """Features de sentença"""
+    feature_df.loc[index,'optimal_sentences'] = asap.nb_optimal_sentence(Y)     #F4
+    
+    """Erros de ortografia"""
+    feature_df.loc[index,'nb_of_spell_errors'] = asap.nb_of_speliing_errors(Y)  #F5
+    feature_df.loc[index,'spell_errors_p_token'] = feature_df['nb_of_spell_errors'][index]/token_feature_tp[0]      #F6
+    
+    """Flesch Score"""
+    feature_df.loc[index,'flesch_score'] = textstat.flesch_reading_ease(Y)      #F7
+    
+    """Distância cosseno entre o essay e o prompt"""
+    feature_df.loc[index,'cos_dist'] = asap.get_cos_dist(Y,valid_set['essay_set'][index])       #F8
+    
+    """Erros gramaticais e de estilo com o LanguageTool"""
+    extract = gg.jvm.languagetoolEnglish.ExtractErrors()
+    errors = extract.getErrors()
+    errors.process(Y)
+    
+    feature_df.loc[index,'style_errors'] = errors.getStyleErrors()              #F9
+    feature_df.loc[index,'grammar_errors'] = errors.getOthersErrors()           #F10
+    feature_df.loc[index,'grammar_errors_p_token'] = feature_df['grammar_errors'][index]/token_feature_tp[0]    #F11
+
+    """Features de marcadores de discurso"""
+    discourse_marker_tp = asap.discourse_markers_features(Y)
+    feature_df.loc[index,'discourse_markers'] = discourse_marker_tp[0]              #F12
+    feature_df.loc[index,'discourse_markers_p_sentence'] = discourse_marker_tp[1]   #F13
     
     print(index)
-    
-    
-feature_df.to_csv('./test.csv', index = False)
+feature_df.to_csv('./ASAP_feature_table.csv', index = False)
